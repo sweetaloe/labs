69add97a043e5198491e1712b0ad86fed8b363a6
sweetaloe <sweetaloe@yandex.ru>
2018-03-31 22:21:30 +0500

    new

--- Л2 - Quick sort/Quick sort/Properties/AssemblyInfo.cs
+++ Л2 - Quick sort/Quick sort/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("QuickSort")]
+[assembly: AssemblyTitle("Quick sort")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuickSort")]
+[assembly: AssemblyProduct("Quick sort")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("b4f27bda-7d41-47bf-b4e7-83b052c3e373")]
+[assembly: Guid("2c5de949-1410-4b05-85b9-6ea9ffbb006b")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
